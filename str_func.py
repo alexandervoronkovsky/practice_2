b8094caa59c8138ed63cd563f8c9da400d13ff72
alexandervoronkovsky <142724496+alexandervoronkovsky@users.noreply.github.com>
2023-09-18 22:18:04 +0500

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,5 +1,14 @@
 def upper_word():
+    """ функция которая принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     string = "hello word"
     print(string.upper())
 
 upper_word()
+
+
+def title_word():
+    """функция которая делает заглавными первые буквы каждого слова в строке"""
+    string = "hello word"
+    print(string.title())
+
+title_word()
